f6b400d44dbb3121843ab1700fb0d98edf3bd295
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-08-09 20:35:51 +0200

    Kész

---
+++ js/person.js
@@ -0,0 +1,3 @@
+'use strict'
+
+ const personDataLog = ( param = { firstname = John , lastname = Doe , age = 33 } = {} ) => "My name is" + param.firstname + param.lastname + '.' + ' I\'m ' + param.age + 'years old.';
